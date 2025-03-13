140644dcb3c48512396b674a18fa03928f019b57
MarlouTadlip <tadlipmarlou@gmail.com>
2025-03-13 11:59:31 +0800

    commite

--- CasaMiro/wwwroot/js/site.js
+++ CasaMiro/wwwroot/js/site.js
@@ -4,3 +4,19 @@
         modal.hide();
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".sidebar-container").classList.add("show");
+});
+
+function toggleSidebar() {
+    document.querySelector(".sidebar-container").classList.toggle("closed");
+}
+
+window.triggerShakeAnimation = () => {
+    let errorMessage = document.getElementById("errorMessage");
+    if (errorMessage) {
+        errorMessage.classList.add("shake");
+        setTimeout(() => errorMessage.classList.remove("shake"), 400);
+    }
+};
---
+++ CasaMiro/wwwroot/js/sweetalert.js
@@ -0,0 +1,36 @@
+﻿// sweetalert.js
+var dotNetReference = null;
+
+function setDotNetReference(reference) {
+    dotNetReference = reference;
+}
+
+function confirmDelete(userId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            dotNetReference.invokeMethodAsync('DeleteUserConfirmed', userId);
+        }
+    });
+}
+
+function showDeleteSuccess() {
+    Swal.fire(
+        'Deleted!',
+        'User has been deleted successfully.',
+        'success'
+    );
+}
+
+function closeModal(modalId) {
+    const modalElement = document.getElementById(modalId);
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+}
